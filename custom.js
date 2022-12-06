657da2cae126f9e93569e71a72d3fdbdc03bd3f2
Duc Ta <t40fpv@gmail.com>
2022-12-05 22:48:22 -0800

    Fixing Errors

--- custom.js
+++ custom.js
@@ -15,7 +15,7 @@ toggle.addEventListener('click', (e) => {
         e.target.innerHTML = 'Dark Mode';
     }else{
         html.classList.add('dark');
-        e.target.innerHTML = 'Light mode';
+        e.target.innerHTML = 'Light Mode';
     }
 })
 
@@ -25,15 +25,16 @@ function setTime(){
     const day = time.getDay();
     const date = time.getDate();
     const hours = time.getHours();
-    const hoursForClock = hours >= 13 ? hours % 12 : hours;
-    const minute = time.getMinutes();
-    const second = time.getSeconds();
+    const hoursForClock = hours % 12;
+    const minutes = time.getMinutes();
+    const seconds = time.getSeconds();
     const ampm = hours >= 12 ? 'PM' : 'AM';
 
-    hourEl.style.transform = 'translate(-50%, -100%) rotate(${scale(hoursForClock, 0, 11, 0, 360)}deg)';
-    minuteEl.style.transform = 'translate(-50%, -100%) rotate(${scale(minutes, 0, 59, 0, 360)}deg)';
-    secondEl.style.transform = 'translate(-50%, -100% rotate(${scale(hoursForClock, 0, 59, 0, 360)}deg)';
-
+    hourEl.style.transform = `translate(-50%, -100%) rotate(${scale(hoursForClock, 0, 11, 0, 360)}deg)`;
+    minuteEl.style.transform = `translate(-50%, -100%) rotate(${scale(minutes, 0, 59, 0, 360)}deg)`;
+    secondEl.style.transform = `translate(-50%, -100%) rotate(${scale(seconds, 0, 59, 0, 360)}deg)`;
+    timeEl.innerHTML = `${hoursForClock}:${minutes < 10 ? `0${minutes}` : minutes} ${ampm}`;
+    dateEl.innerHTML = `${days[day]}, ${months[month]} <span class="circle">${date}</span>`
 }
 
 const scale = (num, in_min, in_max, out_min, out_max) => {
